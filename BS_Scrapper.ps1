1a1f1d953bc6c1e5e932989bbc89cf05147332af
Maxime <134074578+Max51v2@users.noreply.github.com>
2024-11-25 00:31:26 +0100

    Update BS_Scrapper.ps1

--- BS_Scrapper.ps1
+++ BS_Scrapper.ps1
@@ -1,27 +1,39 @@
+#Chemin d'accès des maps BS
 $BSPath = "C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\Beat Saber_Data\CustomLevels"
+
+#Fichier où les musiques seront transferées
 $DestPath = "C:\Users\Maxime\Downloads\test"
 
+#Listage des maps
 Get-ChildItem -Path $BSPath -Directory | ForEach-Object{
 
+    #Chemin de la map
     $LevelPath = Join-Path -Path $BSPath -ChildPath $_.Name
 
+    #Listage du contenu de la map
     Get-ChildItem -Path $LevelPath | ForEach-Object {
+        #Récupération du nom et de l'extension de la musique (format egg)
         if ($_.Extension -ieq ".egg") {
             $SongName = $_.Name
             $Extension = $_.Extension
         }
 
-        # Vérifier les fichiers avec extension .wav
+        #Récupération du nom et de l'extension de la musique (format wav)
         if ($_.Extension -ieq ".wav") {
             $SongName = $_.Name
             $Extension = $_.Extension
         }
     }
 
+    #Nom de la musique (avec format)
     $SongName2 = $_.Name+$Extension
 
+    #Chemin complet vers la musique
     $SongPath = Join-Path -Path $LevelPath -ChildPath $SongName
+
+    #Chemin de destination complet vers la musique
     $SongDestPath = Join-Path -Path $DestPath -ChildPath $SongName2
 
+    #Copie de la musique
     Copy-Item -Path $SongPath -Destination $SongDestPath -Force
 }
