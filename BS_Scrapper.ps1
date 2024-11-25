959ee2f34c375e4ba3f6d9ebebb46dfd28138c55
Maxime <134074578+Max51v2@users.noreply.github.com>
2024-11-25 01:38:36 +0100

    Ajout de la cover

--- BS_Scrapper.ps1
+++ BS_Scrapper.ps1
@@ -1,3 +1,6 @@
+#Installation de FFmpeg s'il ne l'est pas
+winget install ffmpeg
+
 #Chemin d'accès des maps BS
 $BSPath = "C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\Beat Saber_Data\CustomLevels"
 
@@ -10,30 +13,78 @@ Get-ChildItem -Path $BSPath -Directory | ForEach-Object{
     #Chemin de la map
     $LevelPath = Join-Path -Path $BSPath -ChildPath $_.Name
 
+    #Réinitialisation des variables
+    $SongName = $null
+    $SongExtension = $null
+    $ImageName = $null
+    $ImageExtension = $null
+
     #Listage du contenu de la map
     Get-ChildItem -Path $LevelPath | ForEach-Object {
         #Récupération du nom et de l'extension de la musique (format egg)
-        if ($_.Extension -ieq ".egg") {
+        if ($_.Extension -ieq ".egg" || $_.Extension -ieq ".wav") {
             $SongName = $_.Name
-            $Extension = $_.Extension
+            $SongExtension = $_.Extension
         }
 
         #Récupération du nom et de l'extension de la musique (format wav)
-        if ($_.Extension -ieq ".wav") {
-            $SongName = $_.Name
-            $Extension = $_.Extension
+        if ($_.Extension -ieq ".jpg" || $_.Extension -ieq ".png" || $_.Extension -ieq ".jpeg") {
+            $ImageName = $_.Name
+            $ImageExtension = $_.Extension
         }
     }
 
-    #Nom de la musique (avec format)
-    $SongName2 = $_.Name+$Extension
+    
 
-    #Chemin complet vers la musique
-    $SongPath = Join-Path -Path $LevelPath -ChildPath $SongName
-
-    #Chemin de destination complet vers la musique
-    $SongDestPath = Join-Path -Path $DestPath -ChildPath $SongName2
+    
 
     #Copie de la musique
-    Copy-Item -Path $SongPath -Destination $SongDestPath -Force
+    if ($SongName -eq $null) {
+        #Chemin complet vers la musique
+        $SongPath = Join-Path -Path $LevelPath -ChildPath $SongName
+
+        Write-Output "Pas de musique pour : "+$SongPath
+    }
+    else {
+        if($ImageName -eq $null){
+            #Nom de la musique (avec format)
+            $DestSongName = $_.Name+$SongExtension
+
+            #Chemin complet vers la musique
+            $SongPath = Join-Path -Path $LevelPath -ChildPath $SongName
+
+            #Chemin de destination complet vers la musique
+            $SongDestPath = Join-Path -Path $DestPath -ChildPath $DestSongName
+
+            #On la copie au format d'origine
+            Copy-Item -Path $SongPath -Destination $SongDestPath -Force
+        }
+        else {
+            #Nom de la musique (avec format)
+            $DestSongName = $_.Name+".mp4"
+
+            #Chemin complet vers la musique
+            $SongPath = Join-Path -Path $LevelPath -ChildPath $SongName
+
+            #Chemin de destination complet vers la musique
+            $SongDestPath = Join-Path -Path $DestPath -ChildPath $DestSongName
+
+            #Chemin de la cover
+            $CoverPath = Join-Path -Path $LevelPath -ChildPath $ImageName
+
+            #On la copie au format mp4 avec la cover
+            # Commande FFmpeg pour créer un fichier MP4
+            $FFmpegCommand = "ffmpeg -y -loop 1 -i `"$CoverPath`" -i `"$SongPath`" -vf scale=1280:720 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -shortest `"$SongDestPath`""
+
+            try {
+                # Exécuter la commande FFmpeg
+                Write-Host "Création du fichier : $DestSongName"
+                Invoke-Expression $FFmpegCommand
+            } catch {
+                Write-Warning "Erreur lors de la création du fichier $SongName : $_"
+            }
+        }
+    }
 }
+
+Write-Host "En cas d'erreur de FFmpeg, merci de fermer et rouvrir powershell"
